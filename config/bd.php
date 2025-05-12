f0f3fcdf61ad1ee505eec75a83057ab17004b6dc
Zachary <zacbellerose@gmail.com>
2025-05-12 12:08:16 -0400

    idk

--- config/bd.php
+++ config/bd.php
@@ -2,13 +2,12 @@
 
 // charger les variables d'environnement à partir du fichier .env
 require_once get_chemin_defaut('.env.php');
-charger_env(get_chemin_defaut('.env'));
+charger_env(get_chemin_defaut('.env')); 
 
 return [
-    'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'popbazar',
-    'username' => 'root',
-    'password' => ''
+  "host" => getenv('DB_HOST'),
+  "port" => getenv('DB_PORT'),
+  "dbname" => getenv('DB_NAME'),
+  "username" => getenv('DB_USER'),
+  "password" => getenv('DB_PASSWORD'),
 ];
-
