b96c9e1d413e6591809946cae354a838bce3fe6e
Zachary <zacbellerose@gmail.com>
2025-05-16 00:10:01 -0400

    comment pour model

--- models/Categorie.php
+++ models/Categorie.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * Classe Annonce
- * Modèle pour gérer les annonces par categorie dans la base de données
- * 
+ * Classe Categorie
+ * Modèle pour gérer les annonces par catégorie dans la base de données
  */
 class Categorie
 {
@@ -14,9 +13,17 @@ class Categorie
     $config = require get_chemin_defaut('config/bd.php');
     require_once get_chemin_defaut('config/Database.php');
 
-    $this->bd = new Database($config);  // Instance de la classe Database 
+    $this->bd = new Database($config);
   }
 
+  /**
+   * Récupère les annonces associées à une catégorie spécifique avec pagination.
+   *
+   * @param int $idCategorie ID de la catégorie
+   * @param int $limite Nombre d’annonces à afficher par page
+   * @param int $offset Décalage de départ pour la pagination
+   * @return PDOStatement Résultat à traiter avec fetch/fetchAll
+   */
   public function obtenir_annonce_par_categorie($idCategorie, $limite, $offset)
   {
     $sql = "SELECT * FROM produits WHERE categorie_id = ? LIMIT ? OFFSET ?";
@@ -27,13 +34,17 @@ class Categorie
     ]);
   }
 
-public function compter_annonces_par_categorie($idCategorie)
-{
+  /**
+   * Compte le nombre total d’annonces dans une catégorie donnée.
+   *
+   * @param int $idCategorie ID de la catégorie
+   * @return int Nombre d’annonces trouvées
+   */
+  public function compter_annonces_par_categorie($idCategorie)
+  {
     $sql = "SELECT COUNT(*) as total FROM produits WHERE categorie_id = ?";
     $stmt = $this->bd->requete($sql, [1 => $idCategorie]);
     $row = $stmt->fetch();
     return (int)$row['total'];
-}
-
-
+  }
 }
